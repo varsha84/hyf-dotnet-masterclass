9086bac8427e671ce86212864ee601d4880b766e
varsha verma <varsha.verma85@gmail.com>
2022-02-27 11:28:21 +0100

    week1 homework

---
+++ week1/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿//2- String/Math
+//Return the number (count) of vowels in the given string. Consider a, e, i, o, u as vowels.
+
+string input = "Intellectualization";
+int vowelCount = GetVowelCount(input); //TODO: Implement GetVowelCount 
+int GetVowelCount(string input){
+    string str = input.ToLower();
+    int count = 0;
+    foreach(char element in str){
+        if(element == 'a'){
+            count = count + 1;
+        }
+        else if(element == 'e'){
+            count = count + 1;
+        }
+        else if(element == 'i')
+        {
+            count = count + 1;
+        }
+        else if(element == 'o'){
+            count = count + 1;
+        }
+        else if(element == 'u'){
+            count = count + 1;
+        }
+    }
+    return count;
+}
+Console.WriteLine($"Number of vowels: {vowelCount}");
---
+++ week1/task5/Program.cs
@@ -0,0 +1,44 @@
+﻿//5. Arrays
+//Given an integer array as an input, if the length of the array is not even write the warning message, otherwise split the array in half and add both resulting arrays together and write the result.
+
+int[] input = new[] { 1, 2, 5, 7, 2, 3, 5, 7, 9 };
+int[][] splitArray = Split(input);
+int[] result = AddArray(splitArray);
+
+int[][] Split(int[] input){ 
+
+    int len = input.Length/2;
+    int [][] result = new int[2][];
+    result[0] = new int[len];
+    result[1] = new int[len];
+
+    if(((input.Length)%2)!=0){
+        Console.WriteLine($"array lenth is not even");
+    }
+    else{
+       
+        for(int i=0; i<len;i++){
+            result[0][i] = input[i];
+        }
+        for(int i=0; i<len;i++)
+        {
+            result[1][i] = input[i+len];
+        }     
+    }
+    return result;
+}
+
+int [] AddArray(int[][] splitArray){
+    int len =splitArray[0].Length;
+    int [] result = new int[len];
+    for(int i=0; i<len;i++){
+        result[i] = splitArray[0][i] + splitArray[1][i];
+    }   
+    return result;
+}
+
+void WriteResult(int[] result){
+    Console.WriteLine(String.Join(",", result));
+}
+
+WriteResult(result);
