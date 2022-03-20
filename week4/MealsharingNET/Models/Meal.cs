905ef8a574f8439203ce2940ba78b0cb97321980
varsha85 <varsha.verma85@gmail.com>
2022-03-20 13:36:35 +0100

    adding week4 HW

---
+++ week4/MealsharingNET/Models/Meal.cs
@@ -0,0 +1,11 @@
+
+public class Meal{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Cost { get; set; }
+    public string ImageUrl { get; set; }
+    public string Location { get; set; }
+    public int MaxReservations { get; set; }
+    
+}
