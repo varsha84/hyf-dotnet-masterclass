2fff2ddb10a781721ca4e51996818939c57b26bb
varsha85 <varsha.verma85@gmail.com>
2022-05-10 22:08:14 +0200

    update shared.cs

--- week7/MealsharingNET/Shared.cs
+++ week7/MealsharingNET/Shared.cs
@@ -1,4 +1,11 @@
-public class Shared
+/* public class Shared
 {
     public const string ConnectionString = "server=localhost;port=3306;database=meal-sharing;Uid=root;Pwd=root;";
+} */
+
+public class Shared
+{
+  public static string ConnectionString = string.IsNullOrEmpty (Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb")) ? 
+ "server=localhost;port=3306;database=meal-sharing;Uid=root;Pwd=root;Convert Zero Datetime=True"   :
+ Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb");
 }
